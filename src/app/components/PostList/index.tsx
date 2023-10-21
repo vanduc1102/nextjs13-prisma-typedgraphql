382cd4532c3b1549b338e26c973bbe9bd9a8f180
Duc Nguyen <vanducld@gmail.com>
2023-10-21 23:33:37 +0700

    show list user

--- src/app/components/PostList/index.tsx
+++ src/app/components/PostList/index.tsx
@@ -1,14 +1,40 @@
 import { useQuery } from "@apollo/client";
-import React from "react";
+import React, { useState } from "react";
 import { GET_ALL_POST } from "../../queries/getAllPosts";
+import { Post } from "../../../../__generated__/frontend/graphql";
 
 function PostList() {
-  useQuery(GET_ALL_POST, {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  const { loading } = useQuery(GET_ALL_POST, {
     onCompleted(data) {
-      console.log(data);
+      console.log("data?.posts", data?.posts);
+      setPosts(data?.posts || []);
     },
   });
-  return <div>PostList</div>;
+
+  return (
+    <div>
+      {loading ? (
+        "Loading... "
+      ) : (
+        <ol>
+          {posts.map(({ id, title, content, user }) => (
+            <li key={id}>
+              <div>{title}</div>
+              <p>{content}</p>
+              <div>
+                User:{" "}
+                <strong>
+                  {user.name} - {user.email}
+                </strong>
+              </div>
+            </li>
+          ))}
+        </ol>
+      )}
+    </div>
+  );
 }
 
 export default PostList;
--- src/app/queries/getAllPosts.ts
+++ src/app/queries/getAllPosts.ts
@@ -4,11 +4,13 @@ export const GET_ALL_POST = gql`
   query getAllPost {
     posts {
       id
+      title
       content
       createdAt
       updatedAt
       user {
         id
+        name
         email
       }
     }
